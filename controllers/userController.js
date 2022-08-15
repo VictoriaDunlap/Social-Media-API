ca4eb5101145efc1d545d537f1b2e07e8d7ea715
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-08-15 06:44:06 -0400

    uerController created

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,5 +13,50 @@ module.exports = {
         .select('-__v')
         .then((user) => !user ? res.status(404).json({ message: "No user with this ID" })
         : res.json(user))
+    },
+
+    createUser(req, res) {
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+        console.log(err)
+        return res.status(500).json(err)}
+    )},
+
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+        !user 
+            ? res.status(404).json({ message: 'No user with this id' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err))
+    },
+
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) => 
+        !user
+            ? res.status(404).json({ message: 'No user with this id' })
+            : Thought.findOneAndUpdate(
+                { username: req.params.userId }, 
+                { $pull: { username: req.params.userId }}, 
+                { new: true }
+            )
+        )
+        .then((user) => 
+        !thought
+            ? res.json(404).json({
+                message: 'User deleted, but no thoughts were found'})
+            : res.json({ message: 'Student successfully deleted' })
+        )
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json(err)
+        })
     }
 }
